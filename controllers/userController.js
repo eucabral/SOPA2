f1572ea4625c74e76906921e3f9e2535bfb306d5
eucabral <101058486+eucabral@users.noreply.github.com>
2022-10-17 14:44:15 -0300

    feat: login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const createUserToken = require("../helpers/create-user-token");
+const getToken = require("../helpers/get-token");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { name, email, phone, password, confirmpassword, images } = req.body;
@@ -38,4 +40,47 @@ exports.register = async (req, res) => {
   } catch (error) {
     res.status(500).send({ message: error });
   }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(422).send({ message: "Preencha todos os campos!" });
+    return;
+  }
+  //check if user exist
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    res.status(422).send({ message: "Use outro email!" });
+    return;
+  }
+  // check if password match
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ message: "Senha inválida" });
+  }
+
+  await createUserToken(user, req, res);
+};
+
+exports.checkUser = async (req, res) => {
+  let currentUser;
+
+  console.log(req.headers.authorization);
+
+  if (req.headers.authorization) {
+    const token = getToken(req);
+    const decoded = jwt.verify(token, "nossosecret");
+
+    currentUser = await User.findById(decoded.id);
+
+    currentUser.password = undefined;
+  } else {
+    currentUser = null;
+  }
+
+  res.status(200).send(currentUser);
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/checkUser", userController.checkUser);
 
 module.exports = router;
